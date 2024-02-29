b59f0a631c78ab39e9f930fa3d642a7da940d0b9
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-02-29 08:21:09 +0545

    ch

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-kdjfndksjfnjkdsf
+
+let gee;
+const personName = 'sdjdfs';
+
+
+const age = 90;
+const rating = 90.99;
+const isShow = true;
+const bee = false;
+const val = null;
+console.log(personName.repeat(5));
+console.log(personName.repeat(5));
+console.log(val);
